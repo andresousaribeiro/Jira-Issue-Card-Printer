08d2cae3606744013c4b4d31ba3146c3b36f9f15
André Ribeiro <andre.sr@live.com.pt>
2015-09-29 17:56:17 +0100

    Remove file unused

--- bookmarkletLoader.js
+++
@@ -1,8 +0,0 @@
-// tested for Chrome, IE
-javascript:(function(){
-        var head = document.getElementsByTagName("head")[0];
-        var scriptElement = document.createElement("script");
-        scriptElement.src = 'https://qoomon.github.io/Jira-Issue-Card-Printer/bookmarklet.js';	
-        head.appendChild(scriptElement);
-        head.removeChild(scriptElement);
-})();
